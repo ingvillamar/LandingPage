85ff0ab759954fa2d332210c699656571593c665
ingvillamar <jean@example.com>
2026-04-30 16:29:20 -0500

    fix: hero image justify-start to close gap with main div

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -51,7 +51,7 @@ export default function Hero() {
         </div>
 
         {/* Imagen al lado */}
-        <div className="hidden md:flex flex-1 relative h-[480px] justify-center">
+        <div className="hidden md:flex flex-1 relative h-[480px] justify-start">
           <Image
             src="/images/jean-hero.png"
             alt="Architek Specialist"
